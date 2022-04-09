f2b6e946197b711ec950f2a388c76522c34516e7
AJ Jaswal <jaswalakshay@yahoo.com>
2022-04-09 14:23:39 -0400

    added lowercase and uppercase options and variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,24 @@
 // Assignment code here
 function generatePassword() {
   var lowercase = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'];
-  var upercase = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'];
+  var uppercase = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'];
   var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
   var special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
+
+  IncLowercase = confirm("Include lowercase letters? Select OK or Cancel");
+  if (IncLowercase) {
+    alert ("Your password will include lowercase letters");
+  } else {
+    alert ("Your password will NOT include lowercase letters");
+  }
+
+  IncUppercase = confirm("Include uppercase letters? Select OK or Cancel");
+  if (IncUppercase) {
+    alert ("Your password will include uppercase letters");
+  } else {
+    alert ("Your password will NOT include uppercase letters");
+  }
+
 
 }
 
