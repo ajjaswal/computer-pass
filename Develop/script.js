b8ab9b42fdb15a21cf0c85e8768b1f3b833c0f3d
AJ Jaswal <jaswalakshay@yahoo.com>
2022-04-09 13:24:01 -0400

    added characters to function

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,11 @@
 // Assignment code here
+function generatePassword() {
+  var lowercase = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'];
+  var upercase = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'];
+  var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+  var special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
 
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
