663b571d4b80e87a4eb311f43a2fb6a248a8ff0a
AJ Jaswal <jaswalakshay@yahoo.com>
2022-04-16 19:13:33 -0400

    comments for JS

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,4 @@
-// Assignment code here
+// arrays for different character types in potential password
 function generatePassword() {
   var lowercase = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'];
   var uppercase = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'];
@@ -6,6 +6,7 @@ function generatePassword() {
   var special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
   var newpassword = [];
 
+  // Confirm number of characters in password
   numberOfCharacters = prompt("How many characters do you want in your password? Choose between 8-128 characters.");
   if (numberOfCharacters < 8 || numberOfCharacters > 128) {
     return "Invalid. Choose between 8 and 138 characters";
@@ -17,6 +18,7 @@ function generatePassword() {
     
   }
 
+  // variables to decide which characters will be included in the final password
   IncLowercase = confirm("Include lowercase letters? Select OK or Cancel");
   if (IncLowercase) {
     var turnToLowercase = alert("Your password will include lowercase letters");
@@ -45,10 +47,12 @@ function generatePassword() {
     alert("Your password will NOT include Special Characters");
   }
 
+  // alert to tell user to choose at least 1 character type
   if (IncLowercase === false && IncUppercase === false && IncNumbers === false && IncSpecial === false) {
     alert("Choose a character type");
   };
 
+  // Concatenate characters into final password depending what the user chose
   if (IncLowercase) {
     newpassword = newpassword.concat(lowercase);
   }
@@ -63,11 +67,13 @@ function generatePassword() {
     newpassword = newpassword.concat(special);
   }
 
+  // randomize password with math.random and math.floor
   let finalPassword = ""
   for (let i = 0; i < numberOfCharacters; i++) {
     let rng =[Math.floor(Math.random() * newpassword.length)];
     finalPassword = finalPassword + newpassword[rng];
   }
+  // print final password in the box
   return finalPassword;
 }
   
